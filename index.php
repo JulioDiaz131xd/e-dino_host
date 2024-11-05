a57562236f4045f5093516506e7ffa086d3df946
JulioDiaz131xd <prepajulio131@gmail.com>
2024-11-05 11:17:03 -0600

    Change

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ session_start();
             <a href="/../index.php" class="logo">E-Dino</a>
             <div class="header-buttons">
                 <button id="theme-toggle" class="header-btn">
-                    <img id="theme-icon" src="/assets/images/darkmode.png" alt="/assets/images/darkmode.png" />
+                    <img id="theme-icon" src="/assets/images/darkmode.png" alt="5px" />
                 </button>
                 <?php
                 $currentPage = basename($_SERVER['PHP_SELF']);
