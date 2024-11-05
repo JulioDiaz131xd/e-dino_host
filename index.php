f24fe62f4a8487cc55a37238fa529e80484be30f
Daniel Deber <carlosdaniel.debernardi@gmail.com>
2024-11-05 07:56:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,31 @@ session_start();
 
 <body>
     <?php
-    include './includes/header.php';
     include './includes/navbar.php';
     ?>
+<header class="header">
+    <div class="header-container">
+        <a href="/../index.php" class="logo">E-Dino</a>
+        <div class="header-buttons">
+        <button id="theme-toggle" class="header-buttons button">Cambiar Tema</button>
+            <?php
+            $currentPage = basename($_SERVER['PHP_SELF']);
+            if (!in_array($currentPage, ['login.php', 'register.php'])):
+                if (isset($_SESSION['user_id'])): ?>
+                    <form action="/public/logout.php" method="post" class="logout-form">
+                        <button type="submit" class="header-btn">Cerrar sesion</button>
+                    </form>
+                    <div class="user-info"
+                        <span class="user-name"><?php echo htmlspecialchars($_SESSION['nombre']); ?></span>
+                    </div>
+                <?php else: ?>
+                    <button onclick="window.location.href='/../public/login.php'" class="header-btn">Iniciar sesion</button>
+                    <button onclick="window.location.href='/../public/register.php'" class="header-btn">Registrarse</button>
+                <?php endif; 
+            endif; ?>
+        </div>
+    </div>
+</header>
 
     <main>
 
@@ -35,7 +57,6 @@ session_start();
 
         <!-- Sección Beneficios -->
         <section class="benefits slide-left-scroll">
-        <button id="theme-toggle" class="header-buttons button">Cambiar Tema</button>
 
             <h2>Beneficios de E-Dino</h2>
             <div class="benefit-item">
