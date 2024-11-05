6a70b1b4037ca43f88ba2ac0aa9e200df2a2687c
JulioDiaz131xd <prepajulio131@gmail.com>
2024-11-04 21:25:16 -0600

    index

--- index.php
+++ index.php
@@ -22,8 +22,10 @@ session_start();
     <main>
         <!-- Sección Hero -->
         <section class="hero fade-in-scroll">
+            <div class="render">
         <script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.36/build/spline-viewer.js"></script>
         <spline-viewer url="https://prod.spline.design/mjfs-tIONiQcExqV/scene.splinecode"></spline-viewer>
+        </div>
             <?php if (isset($_SESSION['user_id'])): ?>
                 <button id="hero-classes-btn" onclick="window.location.href='./public/dashboard.php'">Ir a mis clases</button>
             <?php else: ?>
