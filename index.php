178d93b0a45fa69c91df7a3d8c77b1d896ff3e6e
Daniel Deber <carlosdaniel.debernardi@gmail.com>
2024-11-06 08:41:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ session_start();
                         <path
                             d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
                     </svg>
-                    <span class="text">Ir a mis clases</span>
+                    <p>Ir a mis cases!</p>
                     <span class="circle"></span>
                     <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
                         <path
@@ -74,7 +74,7 @@ session_start();
                         <path
                             d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
                     </svg>
-                    <span class="text">Registrate!</span>
+                    <p>Registrate!</p>
                     <span class="circle"></span>
                     <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
                         <path
