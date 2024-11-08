ac079ee48e797cb70b8aa38d38d2e10c53a73013
Daniel Deber <carlosdaniel.debernardi@gmail.com>
2024-11-08 08:23:16 -0600

    footer update

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,20 @@
+<link rel="stylesheet" href="./assets/css/style-footer.css">
 <footer class="footer">
     <div class="footer-container">
         <p>© 2024 E-Dino. Todos los derechos reservados.</p>
         <div class="social-icons">
+            <!-- Aquí va el código del ícono -->
+            <div class="loader">
+                <div class="cell d-0"></div>
+                <div class="cell d-1"></div>
+                <div class="cell d-2"></div>
+                <div class="cell d-1"></div>
+                <div class="cell d-2"></div>
+                <div class="cell d-2"></div>
+                <div class="cell d-3"></div>
+                <div class="cell d-3"></div>
+                <div class="cell d-4"></div>
+            </div>
         </div>
     </div>
 </footer>
