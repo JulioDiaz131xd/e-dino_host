1e91a63086eb7c06d6e5ea231f664ab6a4b7ca67
JulioDiaz131xd <prepajulio131@gmail.com>
2024-11-08 19:03:50 -0600

    Update login.php

--- public/login.php
+++ public/login.php
@@ -34,6 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - E-Dino</title>
     <link rel="stylesheet" href="../assets/css/login.css">
+    <link rel="stylesheet" href="../assets/css/header.css">
     <link rel="icon" href="../assets/images/logo.ico">
 </head>
 <body>
