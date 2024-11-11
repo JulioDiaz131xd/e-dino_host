193562bf3c78cefdf8522db9b8695210919eb9a4
Daniel Deber <carlosdaniel.debernardi@gmail.com>
2024-11-11 07:24:37 -0600

    delete {

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="./assets/css/style-footer.css">
 <footer class="footer">
-    <div class="footer-container">}
+    <div class="footer-container">
         <center><p>© 2024 E-Dino. Todos los derechos reservados.</p></center>
         <div class="social-icons">
         </div>
