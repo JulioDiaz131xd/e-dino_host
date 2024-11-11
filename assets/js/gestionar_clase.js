61a87a0b1edbfc80cee40ea51fe02f53b885d3b9
JulioDiaz131xd <prepajulio131@gmail.com>
2024-11-11 17:40:09 -0600

    loading animation

--- assets/js/gestionar_clase.js
+++ assets/js/gestionar_clase.js
@@ -73,3 +73,9 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+window.addEventListener("load", function() {
+    setTimeout(function() {
+      document.querySelector(".loader-wrapper").style.display = "none";
+    }, 2000); 
+});
