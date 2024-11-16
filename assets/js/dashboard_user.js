20f5b12c6f64cfeb7bcd984faaa2462e0e3e2261
JulioDiaz131xd <prepajulio131@gmail.com>
2024-11-16 00:06:09 -0600

    sumary

--- assets/js/dashboard_user.js
+++ assets/js/dashboard_user.js
@@ -66,8 +66,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-window.addEventListener("load", function() {
-    setTimeout(function() {
-      document.querySelector(".loader-wrapper").style.display = "none";
-    }, 2000); 
-});
