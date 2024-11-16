532b5d25a0341fc3efca3a1fe1b66331dea74f26
JulioDiaz131xd <prepajulio131@gmail.com>
2024-11-16 00:38:54 -0600

    Update crear_clase.php

--- public/crear_clase.php
+++ public/crear_clase.php
@@ -43,8 +43,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <nav class="nav-menu">
                     <ul>
                         <li><a href="dashboard.php"><?php echo htmlspecialchars($nombre_usuario); ?></a></li>
-                        <li><a href="logout.php">Cerrar Sesion</a></li>
                         <li><a href="dashboard.php">Volver al Dashboard</a></li>
+                        <li><a href="logout.php">Cerrar Sesion</a></li>
                     </ul>
                 </nav>
             </div>
